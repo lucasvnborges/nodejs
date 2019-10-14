4386ade5ffa7728bd69fb7f23b6792d5c3a4dc3d
Lucas Borges <lucasnsborges@gmail.com>
2019-10-14 18:34:55 -0300

    chore(mongodb): add mongoose connect

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import http from 'http'
-import { env, port, ip, apiRoot } from './config'
+import { env, mongo, port, ip, apiRoot } from './config'
+import mongoose from './services/mongoose'
 import express from './services/express'
 import api from './api'
 
 const app = express(apiRoot, api)
 const server = http.createServer(app)
+
+mongoose.connect(mongo.uri)
+mongoose.Promise = Promise
 
 setImmediate(() => {
   server.listen(port, ip, () => {
