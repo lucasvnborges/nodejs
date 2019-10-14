add8bb7405a878cbe4033393e4c31ea33211b8a0
Lucas Borges <lucasnsborges@gmail.com>
2019-10-14 13:53:39 -0300

    chore(services): add passport configure

---
+++ src/services/passport/index.js
@@ -0,0 +1,39 @@
+import passport from 'passport'
+import { Schema } from 'bodymen'
+import { BasicStrategy } from 'passport-http'
+import { Strategy as BearerStrategy } from 'passport-http-bearer'
+import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt'
+import { jwtSecret, masterKey } from '../../config'
+
+export const password = () => (req, res, next) =>
+  passport.authenticate('password', { session: false }, (err, user, info) => {
+    if (err && err.param) {
+      return res.status(400).json(err)
+    } else if (err || !user) {
+      return res.status(401).end()
+    }
+    req.logIn(user, { session: false }, (err) => {
+      if (err) return res.status(401).end()
+      next()
+    })
+  })(req, res, next)
+
+export const master = () =>
+  passport.authenticate('master', { session: false })
+
+passport.use('master', new BearerStrategy((token, done) => {
+  if (token === masterKey) {
+    done(null, {})
+  } else {
+    done(null, false)
+  }
+}))
+
+passport.use('token', new JwtStrategy({
+  secretOrKey: jwtSecret,
+  jwtFromRequest: ExtractJwt.fromExtractors([
+    ExtractJwt.fromUrlQueryParameter('access_token'),
+    ExtractJwt.fromBodyField('access_token'),
+    ExtractJwt.fromAuthHeaderWithScheme('Bearer')
+  ])
+}))
