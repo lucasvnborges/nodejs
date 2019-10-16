87d664ceff606d8c629c3a2e53ad43f6df3ef46b
Lucas Borges <lucasnsborges@gmail.com>
2019-10-16 02:39:00 -0300

    feat(doc): add apidoc to generate route documentation

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -2,25 +2,67 @@ import { Router } from 'express'
 import { middleware as query } from 'querymen'
 import { middleware as body } from 'bodymen'
 import { password as passwordAuth, master, token } from '../../services/passport'
-import { index, showMe, show, create } from './controller'
+import { index, showMe, show, create, update, updatePassword, destroy } from './controller'
 import { schema } from './model'
 export User, { schema } from './model'
 
 const router = new Router()
 const { email, password, name, picture, role } = schema.tree
 
+/**
+ * @api {get} /users Retrieve users
+ * @apiName RetrieveUsers
+ * @apiGroup User
+ * @apiPermission admin
+ * @apiParam {String} access_token User access_token.
+ * @apiSuccess {Object[]} users List of users.
+ * @apiError {Object} 400 Some parameters may contain invalid values.
+ * @apiError 401 Admin access only.
+ */
 router.get('/',
   token({ required: true, roles: ['admin'] }),
   query(),
   index)
 
+/**
+ * @api {get} /users/me Retrieve current user
+ * @apiName RetrieveCurrentUser
+ * @apiGroup User
+ * @apiPermission user
+ * @apiParam {String} access_token User access_token.
+ * @apiSuccess {Object} user User's data.
+ */
 router.get('/me',
   token({ required: true }),
   showMe)
 
+/**
+ * @api {get} /users/:id Retrieve user
+ * @apiName RetrieveUser
+ * @apiGroup User
+ * @apiPermission public
+ * @apiSuccess {Object} user User's data.
+ * @apiError 404 User not found.
+ */
 router.get('/:id',
   show)
 
+/**
+ * @api {post} /users Create user
+ * @apiName CreateUser
+ * @apiGroup User
+ * @apiPermission master
+ * @apiParam {String} access_token Master access_token.
+ * @apiParam {String} email User's email.
+ * @apiParam {String{6..}} password User's password.
+ * @apiParam {String} [name] User's name.
+ * @apiParam {String} [picture] User's picture.
+ * @apiParam {String=user,admin} [role=user] User's role.
+ * @apiSuccess (Sucess 201) {Object} user User's data.
+ * @apiError {Object} 400 Some parameters may contain invalid values.
+ * @apiError 401 Master access only.
+ * @apiError 409 Email already registered.
+ */
 router.post('/',
   master(),
   body({ email, password, name, picture, role }),
